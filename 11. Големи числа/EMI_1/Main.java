4c592f7f3762c61cb388971e3c253da5f5f62acc
Tsenko Aleksiev <c_aleksiev@abv.bg>
2021-02-21 18:10:40 +0200

    Add files via upload

---
+++ 11. Големи числа/EMI_1/Main.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+// винаги използвайте клас Main, инак ще получите грешка при компилиране!
+public class Main {
+  public static void main(String[] args) {
+    // напишете вашият код тук
+        BigDecimal money = new BigDecimal("10000000");
+        BigDecimal yearlyInterest = new BigDecimal("0.105");
+        BigDecimal valueOfOne = new BigDecimal("1");
+        BigDecimal valueOfTwelve = new BigDecimal("12");
+        BigDecimal monthlyInterest = yearlyInterest.divide(valueOfTwelve);
+        int numberFee = 12 * 10;
+
+
+        BigDecimal firstPart = monthlyInterest.multiply((monthlyInterest.add(valueOfOne).pow(numberFee)));
+        BigDecimal secondPart = (monthlyInterest.add(valueOfOne).pow(numberFee)).subtract(valueOfOne);
+        BigDecimal division = firstPart.divide(secondPart, RoundingMode.HALF_UP);
+        BigDecimal multiplication = money.multiply(division);
+
+        System.out.println(multiplication.setScale(10, RoundingMode.HALF_UP));
+  }
+}
